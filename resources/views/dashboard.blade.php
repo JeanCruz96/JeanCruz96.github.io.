b5f75a0231d0c84bba7fa5bf08f2d405233976a7
Jean Cruz <cruzjean52@gmail.com>
2022-01-14 00:36:14 -0500

    Instalacion y descarga para utilizar font-awesome iconos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,8 +2,22 @@
 
 @section('content')
 
+{{-- <a target="_blank" href="https://www.facebook.com/untelsperu">
+    <img src="{{asset('images/whatsapp.svg')}}" class="img-fluid" width="40px" alt="facebook untels">
+
+</a> --}}
 
 
+{{-- <span style="font-size: 3em; color:green">
+<i class="fas fa-camera"></i>
+    </span>
+ --}}
+
+
+ <a href="https://api.whatsapp.com/send?phone=51928110761&text=Enlace%3A%0Ahttps%3A%2F%2Ffb.me%2Fl2jpqPwlU%0A%0AHe%20visto%20esto%20en%20Facebook...&source_url=https%3A%2F%2Ffb.me%2Fl2jpqPwlU&icebreaker=He%20visto%20esto%20en%20Facebook...&app=facebook&entry_point=post_cta&jid=51928110761%40s.whatsapp.net&context=ARAxAuEjFqgo0KDg4KlXAbJ3cZa1N9KlQFnkF7KENIsT1YzibI7G56ubXHvni7Hmps0YJYc6SRDD1r3HaYVkiE7YkSFfIQrIpDkkmjpv8lOZIVdBA2qW8b21FMw09-tdHFyKm8NfsRgmou6I23XwlZrEeeVyIb91w7XnuajmBMk1Wed8DNqkB7u3nc052yTVSuzekoye41Tv8Y5tEXLF6sbQLzgE3SPuZdLQ_Uc8AqP067HwWj___so&fbclid=IwAR3Zs6Dkjzylr_f95jk3QWRLchccV6qxivETFEKKTrfgQi4NIv3YSBHxdu8" target="_blank"  style="font-size: 3em; color:#50DB24;">
+    <i class="fab fa-whatsapp-square"></i></a>
+<a href="https://www.facebook.com/Inversionesdecofloor/" target="_blank" style="font-size: 3em; color:#274CE3;">
+    <i class="fab fa-facebook-square"></i></a>
 
 
 
