bf338c5efc98bf2001c4cfc0fdfabbc13007200f
Jean Cruz <cruzjean52@gmail.com>
2022-01-20 15:07:50 -0500

    Whatsapp fijo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -136,7 +136,7 @@
                 <li class="list-inline-item footer-menu "><a href="# ">Contáctanos</a></li>
             </ul>
             <ul class="list-inline ">
-                <li class="list-inline-item ">
+                <li class="list-inline-item nav-fijo-ws">
                     <a href="https://api.whatsapp.com/send?phone=51928110761&text=Enlace%3A%0Ahttps%3A%2F%2Ffb.me%2Fl2jpqPwlU%0A%0AHe%20visto%20esto%20en%20Facebook...&source_url=https%3A%2F%2Ffb.me%2Fl2jpqPwlU&icebreaker=He%20visto%20esto%20en%20Facebook...&app=facebook&entry_point=post_cta&jid=51928110761%40s.whatsapp.net&context=ARAxAuEjFqgo0KDg4KlXAbJ3cZa1N9KlQFnkF7KENIsT1YzibI7G56ubXHvni7Hmps0YJYc6SRDD1r3HaYVkiE7YkSFfIQrIpDkkmjpv8lOZIVdBA2qW8b21FMw09-tdHFyKm8NfsRgmou6I23XwlZrEeeVyIb91w7XnuajmBMk1Wed8DNqkB7u3nc052yTVSuzekoye41Tv8Y5tEXLF6sbQLzgE3SPuZdLQ_Uc8AqP067HwWj___so&fbclid=IwAR3Zs6Dkjzylr_f95jk3QWRLchccV6qxivETFEKKTrfgQi4NIv3YSBHxdu8" target="_blank "><img src="images/whatsapp(4).svg " class="img-fluid "></a>
 
                 </li>
