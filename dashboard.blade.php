e2be996d654e6359ff66b84e67a8fc0714e96479
Jean Carlos Cruz Huanca <68082883+JeanCruz96@users.noreply.github.com>
2024-09-23 13:55:00 -0500

    Add files via upload

---
+++ dashboard.blade.php
@@ -0,0 +1,187 @@
+@extends('layout')
+
+@section('content')
+    {{-- <a target="_blank" href="https://www.facebook.com/untelsperu">
+    <img src="{{asset('images/whatsapp.svg')}}" class="img-fluid" width="40px" alt="facebook untels">
+
+</a> --}}
+
+
+
+
+    {{-- <span style="font-size: 3em; color:green">
+<i class="fas fa-camera"></i>
+    </span> --}}
+    <section id="portada">
+        <div class="container">
+
+            <div class="content-center">
+                <br>
+                <p class="sombra"  ><h1 class="mt-5 sombra"><strong>Instalación de Pisos Laminados<br>
+                        O & C Inversiones Decor Floor S.A.C</strong></h1></p>
+                <p class="sombra" style="color:whitesmoke;font-size: 20px"><strong>Instalación, reparación y venta de pisos laminados
+                    </strong> <br>
+                    <strong>Instalación, reparación y venta de zócalos de madera</strong>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    {{-- <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light"></h1>
+            <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+        </div>
+    </div>
+</section> --}}
+    <section id="productos">
+        <div class="bgLightGrey container-fluid">
+            <div id="team" class="content-center">
+                <h2><b>Productos</b></h2>
+            </div>
+        </div>
+
+
+        <div class="album py-4 bg-white">
+            <div class="container">
+                <div class="content-center">
+                    <h2><b>Venta de zócalos de madera matizado</b></h2>
+                </div>
+                <div class="fila">
+                    <div class="columna">
+                        <img src="images/IMG-20210810-WA0004.jpeg" class="" alt="Portfolio 01">
+                        <img src="images/IMG-20201206-WA0003.jpg" class="" alt="Portfolio 01">
+                        <img src="images/164899700_285584239637467_6487346303819565503_n.png" class="" alt="Portfolio 01">
+                    </div>
+                    <div class="columna">
+                    <img src="images/WhatsApp Image 2022-04-27 at 6.14.28 PM.jpeg" class="" alt="Portfolio 01">
+                        <img src="images/Screenshot_2022-02-12-08-30-48.png" class="" alt="Portfolio 01">
+                        <img src="images/WhatsApp Image 2022-04-27 at 6.11.33 PM.jpeg" class="" alt="Portfolio 01">
+                    </div>
+                    <div class="columna">
+                        <img src="images/IMG-20201206-WA0003.jpg" class="" alt="Portfolio 01">
+                        <img src="images/Screenshot_2022-02-12-08-30-30.png" class="" alt="Portfolio 01">
+                        <img src="images/WhatsApp Image 2022-04-27 at 6.14.28 PM (1).jpeg" class="" alt="Portfolio 01">
+                    </div>
+
+                </div>
+                {{-- <div class="col-md-4">
+                <div class="card shadow-sm portfolio-container">
+                    <div class="portfolio-details">
+                    </div>
+                <img src="images/f1.jpeg" class="img-fluid img-thumbnail" alt="Portfolio 01">
+
+                    <div class="card-body">
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
+
+
+                {{-- <div class="col-md-4">
+                <div class="card shadow-sm portfolio-container">
+                    <div class="portfolio-details">
+                    </div>
+                <img src="images/f1.jpeg" class="img-fluid img-thumbnail" alt="Portfolio 01">
+                    <div class="card-body">
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                    <div class="card-body">
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+
+                    <div class="card-body">
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> --}}
+
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <form action="https://formsubmit.co/cruzjean52@gmail.com" method="POST">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mt-4">
+                    <h3> <b>Contáctenos</b></h3>
+                    <p>Déjanos un mensaje. ¿Tienes algún comentario o sugerencia para nosotros? Completa el siguiente formulario para enviarnos lo que nos quieras decir.
+                    </p>
+                </div>
+                <div class="col-md-6 mt-4">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input type="text" required  name="nombres" class="form-control" id="nombres" placeholder="Nombres Completos">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input type="email"required name="correo"  class="form-control" id="correo" placeholder="Correo Electrónico">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <textarea type="textarea"  name="mensaje" required class="form-control" id="mensaje" placeholder="Mensaje"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input type="number" name="telefono" pattern="/^-?\d+\.?\d*$/" onKeyPress="if(this.value.length==9) return false;" class="form-control" id="telefono" placeholder="Numero Telefónico (opcional)">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <button type="submit" href="" class="btn2 btn-primary2 full-width2">Enviar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    </section>
+
+
+    <!-- Otra forma de generar iconos con fontawesome-->
+    {{-- <a href="https://api.whatsapp.com/send?phone=51928110761&text=Enlace%3A%0Ahttps%3A%2F%2Ffb.me%2Fl2jpqPwlU%0A%0AHe%20visto%20esto%20en%20Facebook...&source_url=https%3A%2F%2Ffb.me%2Fl2jpqPwlU&icebreaker=He%20visto%20esto%20en%20Facebook...&app=facebook&entry_point=post_cta&jid=51928110761%40s.whatsapp.net&context=ARAxAuEjFqgo0KDg4KlXAbJ3cZa1N9KlQFnkF7KENIsT1YzibI7G56ubXHvni7Hmps0YJYc6SRDD1r3HaYVkiE7YkSFfIQrIpDkkmjpv8lOZIVdBA2qW8b21FMw09-tdHFyKm8NfsRgmou6I23XwlZrEeeVyIb91w7XnuajmBMk1Wed8DNqkB7u3nc052yTVSuzekoye41Tv8Y5tEXLF6sbQLzgE3SPuZdLQ_Uc8AqP067HwWj___so&fbclid=IwAR3Zs6Dkjzylr_f95jk3QWRLchccV6qxivETFEKKTrfgQi4NIv3YSBHxdu8" target="_blank"  style="font-size: 3em; color:#50DB24;">
+    <i class="fab fa-whatsapp-square"></i></a>
+<a href="https://www.facebook.com/Inversionesdecofloor/" target="_blank" style="font-size: 3em; color:#274CE3;">
+    <i class="fab fa-facebook-square"></i></a> --}}
+@endsection
